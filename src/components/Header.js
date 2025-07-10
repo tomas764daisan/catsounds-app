b45c52c95f8238ff25f946fd1b8c417c59030d3e
tomas764daisan <tomas764daisan@outlook.com>
2025-07-10 11:04:55 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>🐾 CatSounds App</h1>
+      <p>Track your meow-moments & enjoy purring sounds!</p>
+    </header>
+  );
+}
+
+export default Header;
